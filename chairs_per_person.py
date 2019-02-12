12791265a963585a29ced573dc9e22bf2d36473b
Aqissiaq <aqissiaq@gmail.com>
2019-02-12 15:19:35 +0100

    Added error handling
    Added handling of exceptions for reservations of zero seats, non-number values and reservations with no names

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,14 @@
-f = open("reservations.csv")
-
-for reservation in f:
-    name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
-    print("{} will get {} chairs per person".format(name, chairs_per_person))
+with open("reservations.csv") as f:
+    for reservation in f:
+        name, number = reservation.split(",")
+        try:
+            chairs_per_person = 50 / int(number)
+            assert name is not ""
+        except ZeroDivisionError:
+            print("{} has attempted to reserve 0 seats".format(name))
+        except ValueError:
+            print("{} has reserved a non-number amount of seats".format(name))
+        except AssertionError:
+            print("Name must be a non-empty string")
+            
+        print("{} will get {} chairs per person".format(name, chairs_per_person))
